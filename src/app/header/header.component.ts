4009af7b4051724e49a6be72b3d7a5ef90d07142
Lasse Lentz Thomsen <kontakt@lasselentz.dk>
2017-09-03 00:47:21 +0200

    Refactoring of header component
    Moved header-menu inside header component directly

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HeaderMenuComponent } from '../header-menu/header-menu.component';
 
 @Component({
 	selector: 'app-header',
@@ -8,6 +7,7 @@ import { HeaderMenuComponent } from '../header-menu/header-menu.component';
 })
 export class HeaderComponent implements OnInit {
 	logoPaths: object;
+	menuItems: object;
 
 	constructor() { }
 
@@ -17,6 +17,41 @@ export class HeaderComponent implements OnInit {
 			x2 : '/assets/img/general/logo@2x.png',
 			x3 : '/assets/img/general/logo@3x.png'
 		}; 
+
+		this.menuItems = [
+			{
+				text  : 'Login',
+				icons : [
+					'/assets/img/icons/login.png',
+					'/assets/img/icons/login@2x.png',
+					'/assets/img/icons/login@3x.png'
+				]
+			},
+			{
+				text  : 'Items',
+				icons : [
+					'/assets/img/icons/bag.png',
+					'/assets/img/icons/bag@2x.png',
+					'/assets/img/icons/bag@3x.png'
+				]
+			},
+			{
+				text  : null,
+				icons : [
+					'/assets/img/icons/search.png',
+					'/assets/img/icons/search@2x.png',
+					'/assets/img/icons/search@3x.png'
+				]
+			},
+			{
+				text  : null,
+				icons : [
+					'/assets/img/icons/menu.png',
+					'/assets/img/icons/menu@2x.png',
+					'/assets/img/icons/menu@3x.png'
+				]
+			},
+		];
 	}
 
 }
