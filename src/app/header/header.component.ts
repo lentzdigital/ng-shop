36ed995b8e0cbb6a5fe40cfb1a47c23bf7d9236d
Lasse Lentz Thomsen <kontakt@lasselentz.dk>
2017-09-08 05:22:59 +0200

    Small fixes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -43,34 +43,34 @@ export class HeaderComponent implements OnInit {
 		});
 
 		this.logoPaths = {
-			x1 : '/assets/img/general/logo.png',
-			x2 : '/assets/img/general/logo@2x.png',
-			x3 : '/assets/img/general/logo@3x.png'
+			x1 : 'assets/img/general/logo.png',
+			x2 : 'assets/img/general/logo@2x.png',
+			x3 : 'assets/img/general/logo@3x.png'
 		}; 
 
 		this.menuItems = [
 			{
 				text  : 'Login',
 				icons : [
-					'/assets/img/icons/login.png',
-					'/assets/img/icons/login@2x.png',
-					'/assets/img/icons/login@3x.png'
+					'assets/img/icons/login.png',
+					'assets/img/icons/login@2x.png',
+					'assets/img/icons/login@3x.png'
 				]
 			},
 			{
 				text  : 'Items',
 				icons : [
-					'/assets/img/icons/bag.png',
-					'/assets/img/icons/bag@2x.png',
-					'/assets/img/icons/bag@3x.png'
+					'assets/img/icons/bag.png',
+					'assets/img/icons/bag@2x.png',
+					'assets/img/icons/bag@3x.png'
 				]
 			},
 			{
 				text  : null,
 				icons : [
-					'/assets/img/icons/search.png',
-					'/assets/img/icons/search@2x.png',
-					'/assets/img/icons/search@3x.png'
+					'assets/img/icons/search.png',
+					'assets/img/icons/search@2x.png',
+					'assets/img/icons/search@3x.png'
 				]
 			}
 		];
